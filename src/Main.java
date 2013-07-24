fefdc869c23f665e0d0265450b7e1b6f4dc21c02
A. Pichugin <01.datalink@gmail.com>
2013-07-25 00:13:23 +0400

    added email addres for code review

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,9 @@ import com.datalink.cellgames.xo.*;
  * Date: 10.07.13
  * Time: 17:26
  */
+
+// mailto: 01.datalink@gmail.com
+
 public class Main {
     public static void main(String[] args) {
         GameXO g = new GameXO();
